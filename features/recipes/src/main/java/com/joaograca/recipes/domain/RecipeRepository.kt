7106a2f020fa9b1eac05f6f76c851d1c76d4346d
João Graça <joaograca360@gmail.com>
2022-07-19 22:23:19 +0100

    Add return types to repository methods

--- features/recipes/src/main/java/com/joaograca/recipes/domain/RecipeRepository.kt
+++ features/recipes/src/main/java/com/joaograca/recipes/domain/RecipeRepository.kt
@@ -5,9 +5,9 @@ import com.joaograca.recipes.domain.model.Recipe
 
 interface RecipeRepository {
 
-    suspend fun searchRecipe(keyword: String)
+    suspend fun searchRecipe(keyword: String): Result<List<Recipe>>
 
-    suspend fun searchRecipeByIngredient(ingredient: Ingredient)
+    suspend fun searchRecipeByIngredient(ingredient: Ingredient): Result<List<Recipe>>
 
-    suspend fun createRecipe(recipe: Recipe)
+    suspend fun createRecipe(recipe: Recipe): Result<Unit>
 }
