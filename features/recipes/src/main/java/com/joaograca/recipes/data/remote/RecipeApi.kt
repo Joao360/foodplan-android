1baabe695836b26652bd11c6d90d4d9a995acbd6
João Graça <joaograca360@gmail.com>
2022-08-21 13:17:36 +0100

    build: Add spoonacular api key to make requests

--- features/recipes/src/main/java/com/joaograca/recipes/data/remote/RecipeApi.kt
+++ features/recipes/src/main/java/com/joaograca/recipes/data/remote/RecipeApi.kt
@@ -1,5 +1,6 @@
 package com.joaograca.recipes.data.remote
 
+import com.joaograca.recipes.BuildConfig
 import com.joaograca.recipes.data.remote.dto.ComplexSearchDto
 import com.joaograca.recipes.data.remote.dto.RecipeDto
 import retrofit2.http.GET
@@ -7,19 +8,18 @@ import retrofit2.http.Query
 
 interface RecipeApi {
 
-    @GET("recipes/complexSearch")
+    @GET("recipes/complexSearch?apiKey=${API_KEY}")
     suspend fun searchRecipe(
-        @Query("apiKey") apiKey: String,
         @Query("query") keyword: String
     ): ComplexSearchDto
 
-    @GET("recipes/findByIngredients")
+    @GET("recipes/findByIngredients?apiKey=${API_KEY}")
     suspend fun searchRecipeByIngredient(
-        @Query("apiKey") apiKey: String,
         @Query("ingredients") ingredients: String
     ): RecipeDto
 
     companion object {
         const val BASE_URL = "https://api.spoonacular.com/"
+        const val API_KEY = BuildConfig.API_KEY
     }
 }
