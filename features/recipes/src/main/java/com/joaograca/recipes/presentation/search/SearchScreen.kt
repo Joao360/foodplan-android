43936bee979e45c3b014b39b8d64c23b8a7a931d
João Graça <jonymiguelgraca@gmail.com>
2022-10-04 12:59:10 +0100

    feat: Add SearchTextField to query API about recipes

--- features/recipes/src/main/java/com/joaograca/recipes/presentation/search/SearchScreen.kt
+++ features/recipes/src/main/java/com/joaograca/recipes/presentation/search/SearchScreen.kt
@@ -1,8 +1,14 @@
 package com.joaograca.recipes.presentation.search
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.joaograca.core_ui.LocalSpacing
 import com.joaograca.recipes.presentation.search.component.SearchTextField
 
 @Composable
@@ -10,7 +16,18 @@ fun SearchScreen(
     viewModel: SearchViewModel = hiltViewModel()
 ) {
     val state = viewModel.uiState.collectAsState().value
+    val spacing = LocalSpacing.current
 
-
-    SearchTextField(text = state.query, onValueChange = viewModel::onQueryChange)
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = spacing.spaceMedium, vertical = spacing.spaceLarge)
+    ) {
+        SearchTextField(
+            text = state.query,
+            onValueChange = viewModel::onQueryChange,
+            onSearch = viewModel::onSearch,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
--- features/recipes/src/main/java/com/joaograca/recipes/presentation/search/component/SearchTextField.kt
+++ features/recipes/src/main/java/com/joaograca/recipes/presentation/search/component/SearchTextField.kt
@@ -1,12 +1,35 @@
 package com.joaograca.recipes.presentation.search.component
 
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun SearchTextField(
     text: String,
-    onValueChange: (String) -> Unit
+    onValueChange: (String) -> Unit,
+    onSearch: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    TextField(value = text, onValueChange = onValueChange)
+    TextField(
+        value = text,
+        onValueChange = onValueChange,
+        singleLine = true,
+        keyboardActions = KeyboardActions(
+            onSearch = {
+                onSearch()
+                defaultKeyboardAction(ImeAction.Search)
+            }
+        ),
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Search
+        ),
+        modifier = modifier.clip(RoundedCornerShape(5.dp)),
+    )
 }
