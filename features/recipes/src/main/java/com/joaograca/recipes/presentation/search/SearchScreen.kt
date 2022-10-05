624c989fb118298fbd6b79a927a088f0f827e15e
João Graça <jonymiguelgraca@gmail.com>
2022-10-05 14:43:53 +0100

    feat: Replace lazy column for grid and add icons for image placeholder and error

--- features/recipes/src/main/java/com/joaograca/recipes/presentation/search/SearchScreen.kt
+++ features/recipes/src/main/java/com/joaograca/recipes/presentation/search/SearchScreen.kt
@@ -1,8 +1,9 @@
 package com.joaograca.recipes.presentation.search
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -11,8 +12,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.joaograca.core_ui.LocalSpacing
+import com.joaograca.recipes.domain.model.RecipePreview
 import com.joaograca.recipes.presentation.search.component.RecipeListItem
 import com.joaograca.recipes.presentation.search.component.SearchTextField
 
@@ -21,6 +25,15 @@ fun SearchScreen(
     viewModel: SearchViewModel = hiltViewModel()
 ) {
     val state = viewModel.uiState.collectAsState().value
+    SearchScreenContent(state, viewModel::onQueryChange, viewModel::onSearch)
+}
+
+@Composable
+private fun SearchScreenContent(
+    state: SearchUiState,
+    onValueChange: (String) -> Unit,
+    onSearch: () -> Unit
+) {
     val spacing = LocalSpacing.current
 
     Column(
@@ -34,17 +47,21 @@ fun SearchScreen(
     ) {
         SearchTextField(
             text = state.query,
-            onValueChange = viewModel::onQueryChange,
-            onSearch = viewModel::onSearch,
+            onValueChange = onValueChange,
+            onSearch = onSearch,
             modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.height(spacing.spaceSmall))
-        LazyColumn(
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 128.dp),
             modifier = Modifier
                 .fillMaxSize()
         ) {
             items(state.recipes) { recipe ->
-                RecipeListItem(recipe)
+                RecipeListItem(
+                    recipe = recipe,
+                    modifier = Modifier.aspectRatio(1f)
+                )
             }
         }
     }
@@ -63,4 +80,21 @@ fun SearchScreen(
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    val state = SearchUiState(
+        query = "Tomato",
+        isSearching = false,
+        recipes = listOf(
+            RecipePreview(id = 1, name = "Tomato sauce", imageUrl = ""),
+            RecipePreview(id = 1, name = "Neque porro quisquam ", imageUrl = ""),
+            RecipePreview(id = 1, name = "Excepteur sint occaecat cupidatat", imageUrl = ""),
+            RecipePreview(id = 1, name = "incididunt ", imageUrl = ""),
+        )
+    )
+
+    SearchScreenContent(state = state, onSearch = {}, onValueChange = {})
 }
--- features/recipes/src/main/java/com/joaograca/recipes/presentation/search/component/RecipeListItem.kt
+++ features/recipes/src/main/java/com/joaograca/recipes/presentation/search/component/RecipeListItem.kt
@@ -1,56 +1,76 @@
 package com.joaograca.recipes.presentation.search.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.joaograca.core_ui.LocalSpacing
+import com.joaograca.recipes.R
 import com.joaograca.recipes.domain.model.RecipePreview
 
 @Composable
 fun RecipeListItem(
-    recipe: RecipePreview
+    recipe: RecipePreview,
+    modifier: Modifier
 ) {
     val spacing = LocalSpacing.current
 
     Column(
-        modifier = Modifier
-            .fillMaxWidth()
+        modifier = modifier
+            .clip(RoundedCornerShape(spacing.spaceMedium))
             .padding(spacing.spaceExtraSmall)
-            .clip(RoundedCornerShape(5.dp))
-            .shadow(elevation = 1.dp, shape = RoundedCornerShape(5.dp))
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(spacing.spaceMedium)
-        ) {
-            AsyncImage(
-                model = recipe.imageUrl,
-                contentDescription = null,
-                modifier = Modifier.size(100.dp)
+            .shadow(
+                elevation = 2.dp,
+                shape = RoundedCornerShape(spacing.spaceMedium)
             )
-            Spacer(modifier = Modifier.width(spacing.spaceMedium))
-            Text(recipe.name, style = MaterialTheme.typography.h6)
-        }
+            .background(MaterialTheme.colors.surface)
+            .padding(spacing.spaceMedium),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AsyncImage(
+            model = recipe.imageUrl,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(100.dp)
+                .clip(RoundedCornerShape(5.dp)),
+            placeholder = painterResource(id = R.drawable.ic_tray_plate),
+            error = painterResource(id = R.drawable.ic_error),
+        )
+        Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        Text(
+            text = recipe.name,
+            style = MaterialTheme.typography.body1,
+            maxLines = 2,
+            textAlign = TextAlign.Center,
+            overflow = TextOverflow.Ellipsis
+        )
     }
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
-    val recipe = RecipePreview(
-        id = 1,
-        "Tomato Pizza",
-        imageUrl = ""
+    RecipeListItem(
+        recipe = RecipePreview(
+            id = 1,
+            "Lorem ipsum dolor sit amet",
+            imageUrl = ""
+        ),
+        modifier = Modifier.aspectRatio(1f)
     )
-
-    RecipeListItem(recipe = recipe)
 }
