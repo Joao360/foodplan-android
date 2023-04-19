8d067a44ec8d96a8d970e20ced229babfa9accc3
João Graça <joaograca360@gmail.com>
2023-04-19 19:46:17 +0100

    refactor: Give proper test data to complex search dto

--- features/recipes/src/test/java/com/joaograca/recipes/data/remote/dto/ComplexSearchItemDtoFactory.kt
+++ features/recipes/src/test/java/com/joaograca/recipes/data/remote/dto/ComplexSearchItemDtoFactory.kt
@@ -2,9 +2,9 @@ package com.joaograca.recipes.data.remote.dto
 
 object ComplexSearchItemDtoFactory {
     fun create(): ComplexSearchItemDto = ComplexSearchItemDto(
-        id = 1,
-        title = "",
-        image = "",
-        imageType = ""
+        id = 716429,
+        title = "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
+        image = "https://spoonacular.com/recipeImages/716429-312x231.jpg",
+        imageType = "jpg"
     )
 }
